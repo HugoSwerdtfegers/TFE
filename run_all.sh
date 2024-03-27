16825b06fe56c383f8afa2e8df50512917147d76
Hugo Swerdtfegers <130606953+HugoSwerdtfegers@users.noreply.github.com>
2024-03-27 12:37:10 +0100

    Add files via upload

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 for config in 1 2 3; do
     for scenario in reference optimistic conservative; do
-        echo "Lancement de la simulation pour config $config et scenario $scenario"    
+        echo "Simulation launch for config $config and $scenario scenario"    
         python3 main.py --config $config --scenario $scenario
         echo ""
     done
